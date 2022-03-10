513cb47bac95ecca676ca9ab40ecdc3073e11cf1
shengningyi <287369241@qq.com>
2022-03-10 18:10:45 +0800

    add cookie

--- src/main/java/com/sny/community/controller/IndexController.java
+++ src/main/java/com/sny/community/controller/IndexController.java
@@ -1,13 +1,36 @@
 package com.sny.community.controller;
 
+import com.sny.community.mapper.UserMapper;
+import com.sny.community.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.annotation.Resource;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class IndexController {
 
+    @Resource
+    UserMapper userMapper;
+
     @GetMapping("/")
-    public String hello(){
+    public String hello(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        if(cookies != null){
+            for (Cookie cookie:cookies
+            ) {
+                if(cookie.getName().equals("token")){
+                    String token = cookie.getValue();
+                    User user =  userMapper.findByToken(token);
+                    if(user != null){
+                        request.getSession().setAttribute("user", user);
+                    }
+                    break;
+                }
+            }
+        }
         return "index";
     }
 }
