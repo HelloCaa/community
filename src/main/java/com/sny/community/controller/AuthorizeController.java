2c34545d062adcfab6de65a453353da6f3df9d2b
shengningyi <287369241@qq.com>
2022-03-10 16:54:46 +0800

    3.10 17

--- src/main/java/com/sny/community/controller/AuthorizeController.java
+++ src/main/java/com/sny/community/controller/AuthorizeController.java
@@ -2,6 +2,8 @@ package com.sny.community.controller;
 
 import com.sny.community.dto.AccessTokenDTO;
 import com.sny.community.dto.GitHubUser;
+import com.sny.community.mapper.UserMapper;
+import com.sny.community.model.User;
 import com.sny.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -26,6 +29,9 @@ public class AuthorizeController {
     @Value("${github.redirect.uri}")
     private String clientUri;
 
+    @Resource
+    private UserMapper userMapper;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state")  String state,
@@ -37,10 +43,17 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_uri(clientUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GitHubUser user = githubProvider.getUser(accessToken);
-        if(user != null){
+        GitHubUser gitHubUser = githubProvider.getUser(accessToken);
+        if(gitHubUser != null){
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(gitHubUser.getName());
+            user.setAccountId(String.valueOf(gitHubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
             //登陆成功，写cookie和session
-            request.getSession().setAttribute("user", user);
+            request.getSession().setAttribute("user", gitHubUser);
             return "redirect:/";
         } else {
             //登陆失败，重新登陆
