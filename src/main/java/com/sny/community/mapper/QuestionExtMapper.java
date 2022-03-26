69ed655d0d7fa1479a7da3cdce37bbf3026ca2bc
shengningyi <287369241@qq.com>
2022-03-26 19:49:25 +0800

    完成相关问题展示（有bug）

--- src/main/java/com/sny/community/mapper/QuestionExtMapper.java
+++ src/main/java/com/sny/community/mapper/QuestionExtMapper.java
@@ -10,4 +10,5 @@ import java.util.List;
 public interface QuestionExtMapper {
     int incView(Question row);
     int incCommentCount(Question record);
+    List<Question> selectRelated(Question question);
 }
--- src/main/java/com/sny/community/service/QuestionService.java
+++ src/main/java/com/sny/community/service/QuestionService.java
@@ -10,13 +10,16 @@ import com.sny.community.mapper.UserMapper;
 import com.sny.community.model.Question;
 import com.sny.community.model.QuestionExample;
 import com.sny.community.model.User;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.ibatis.session.RowBounds;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.StringJoiner;
 import java.util.stream.Collectors;
 
 @Service
@@ -49,8 +52,9 @@ public class QuestionService {
         Integer offset = size * (page - 1);
 
         //调用dao层获取数据
-        List<Question> questions = questionMapper.selectByExampleWithBLOBsWithRowbounds(new QuestionExample(), new RowBounds(offset, size));
-
+        QuestionExample questionExample = new QuestionExample();
+        questionExample.setOrderByClause("gmt_create desc");
+        List<Question> questions = questionMapper.selectByExampleWithBLOBsWithRowbounds(questionExample, new RowBounds(offset, size));
         List<QuestionDTO> questionDTOS = new ArrayList<>();
         for (Question question:questions
              ) {
@@ -134,5 +138,24 @@ public class QuestionService {
         question.setId(id);
         question.setViewCount(1);
         questionExtMapper.incView(question);
+    }
+
+    public List<QuestionDTO> selectRelated(QuestionDTO queryDTO) {
+        if(StringUtils.isBlank(queryDTO.getTag())){
+            return new ArrayList<>();
+        }
+        String[] tags = StringUtils.split(queryDTO.getTag(), ",");
+        String regexpTag = Arrays.stream(tags).collect(Collectors.joining("|"));
+        Question question = new Question();
+        question.setId(queryDTO.getId());
+        question.setTag(regexpTag);
+
+        List<Question> questions = questionExtMapper.selectRelated(question);
+        List<QuestionDTO> questionDTOS = questions.stream().map(q -> {
+            QuestionDTO questionDTO = new QuestionDTO();
+            BeanUtils.copyProperties(q, questionDTO);
+            return questionDTO;
+        }).collect(Collectors.toList());
+        return questionDTOS;
     }
 }
