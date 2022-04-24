ea7983be2b079838769f3ee2ceea9699350002d0
shengningyi <287369241@qq.com>
2022-04-24 19:24:00 +0800

    准备发布

--- src/main/java/com/sny/community/controller/AuthorizeController.java
+++ src/main/java/com/sny/community/controller/AuthorizeController.java
@@ -5,6 +5,7 @@ import com.sny.community.dto.GitHubUser;
 import com.sny.community.model.User;
 import com.sny.community.provider.GithubProvider;
 import com.sny.community.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +18,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
+@Slf4j
 public class AuthorizeController {
 
     @Resource
@@ -71,6 +73,7 @@ public class AuthorizeController {
             response.addCookie(new Cookie("token", token));
             return "redirect:/";
         } else {
+            log.error("callback get github error,{}",gitHubUser);
             //登陆失败，重新登陆
             return "redirect:/";
         }
